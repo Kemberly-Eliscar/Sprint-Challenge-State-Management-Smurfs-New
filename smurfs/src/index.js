362dd00f4616bc7435b1f94e0078bf5036eeec2f
Kemberly Eliscar <kemberlyeliscar@Kemberlys-Air-3.home>
2019-11-05 17:01:54 -0500

    basic reducer set up

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
 import "./index.css";
 import App from "./components/App";
+import { Provider } from 'react-redux';
 
-const store = createStore(reducer, applyMiddleware(thunk));
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//redux
+import { createStore, applyMiddleware, compose } from "redux";
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { reducer } from './reducers'
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk, logger)))
+
+
+ReactDOM.render(
+    <Provider store = { store }>
+        <App />
+    </Provider>, 
+    document.getElementById("root"));

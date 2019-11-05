1afbdb9e7b48af60b936bd885b125eba921e6985
Kemberly Eliscar <kemberlyeliscar@Kemberlys-Air-3.home>
2019-11-05 16:12:17 -0500

    initial commit + added middleware

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import "./index.css";
 import App from "./components/App";
+
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(<App />, document.getElementById("root"));
